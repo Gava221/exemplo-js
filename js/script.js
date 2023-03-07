866ce1dcfeef899cbdb9c89361f1a90e55b5a413
Gava221 <rm550595@fiap.com.br>
2023-03-07 11:49:06 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Este é um comentario de linha
+
+/**
+ * e aqui é 
+ * um comentario
+ * de bloco
+ */
+
+console.log("olá mundo!")
+console.error("Bom dia!")
+console.warn("Sera?")
+console.info("vai ser...")
